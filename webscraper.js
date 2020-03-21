935b7f75b228d7e4bd1eb79cd26a942da9d3cedc
Michal P <16662498@students.lincoln.ac.uk>
2020-03-21 14:06:40 +0000

    Fixed issue with cross-origin

--- webscraper.js
+++ webscraper.js
@@ -1,10 +1,26 @@
+var url = "https://www.whateverorigin.org/get?url=" + encodeURIComponent("https://www.waitrose.com/ecom/products/essential-waitrose-semi-skimmed-milk/053457-26759-26760" + "&callback=?");
+var settings = {
+          'cache': false,
+          'dataType': "jsonp",
+          "async": true,
+          "crossDomain": true,
+          "url": url,
+          "method": "GET",
+          "headers": {
+              "accept": "application/json",
+              "Access-Control-Allow-Origin":"*"
+          }
+}
+var urlObject;
+
 $(document).ready(function() {
 
-  var url = "https://www.whateverorigin.org/get?url=" + encodeURIComponent("https://www.waitrose.com/ecom/products/essential-waitrose-semi-skimmed-milk/053457-26759-26760" + "&callback=?");
-  $.getJSON(url, function(response)
-    {
+  $.ajax(settings).done(function (response) {
+      urlObject = JSON.stringify(response);
       console.log(response);
-    }
-  );
+      //alert(urlObject.match(/<span data-test="product-pod-price">\[ ([\d]*?) \]<\/span>/)[1]);
+  });
+
+
 
 });
