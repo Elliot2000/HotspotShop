2df1473f3724f3109983e666ecf06a6b18314f26
Michal P <16662498@students.lincoln.ac.uk>
2020-03-21 15:30:12 +0000

    Created webscraper for first product

--- webscraper.js
+++ webscraper.js
@@ -1,4 +1,4 @@
-var url = "https://www.whateverorigin.org/get?url=" + encodeURIComponent("https://www.waitrose.com/ecom/products/essential-waitrose-semi-skimmed-milk/053457-26759-26760" + "&callback=?");
+var url = "https://www.whateverorigin.org/get?url=" + encodeURIComponent("https://www.waitrose.com/ecom/products/essential-waitrose-semi-skimmed-milk/053457-26759-26760");
 var settings = {
           'cache': false,
           'dataType': "jsonp",
@@ -12,15 +12,30 @@ var settings = {
           }
 }
 var urlObject;
+var numberID;
+var price;
 
 $(document).ready(function() {
 
   $.ajax(settings).done(function (response) {
       urlObject = JSON.stringify(response);
-      console.log(response);
-      //alert(urlObject.match(/<span data-test="product-pod-price">\[ ([\d]*?) \]<\/span>/)[1]);
+      numberID = urlObject.search('<span>£');
+      price = urlObject[numberID+6] + urlObject[numberID+7] + urlObject[numberID+8] + urlObject[numberID+9] + urlObject[numberID+10];
+      if(price != "tents"){
+        $(".skimmed_milk_2").text(price);
+      }else{
+            $( ".overlay2" ).fadeIn( "slow",
+            function() {
+              $( ".overlay2" ).delay(5000).fadeOut("slow");
+            });
+      }
+
   });
+});
 
 
 
+$( ".close" ).click(function(event) {
+  event.preventDefault();
+  $( ".overlay2" ).hide();
 });
