f4c149e20fb746cce681868997186ac5d1f54ae6
Michal P <16662498@students.lincoln.ac.uk>
2020-04-07 20:27:48 +0100

    Added splash screen

---
+++ app/src/main/java/com/example/hotspotshop/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.example.hotspotshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SplashScreen extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainPicker();
+            }
+        });
+    }
+
+    public void openMainPicker(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
